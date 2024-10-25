900b85f9e15269c3a99599b20f2c06769a3acfc0
Derek Hayes <derek2099@gmail.com>
2024-10-25 15:43:22 -0500

    Add interactive dots

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -2,6 +2,7 @@ const slideshow = () => {
   let index = 0;
   const next = document.querySelector(".next");
   const prev = document.querySelector(".prev");
+  const dots = document.querySelectorAll(".dot");
 
   const showSlide = (slideIndex) => {
     const slides = document.querySelectorAll(".slide");
@@ -18,21 +19,35 @@ const slideshow = () => {
       slideIndex = slides.length - 1;
       index = slides.length - 1;
     }
-    console.log('slideindex:', slideIndex)
     slides[slideIndex].style.display = "block";
+
+    // dots
+
+    dots.forEach((dot) => {
+      dot.style.backgroundColor = "#444";
+    });
+
+    dots[slideIndex].style.backgroundColor = "#46f511";
   };
 
   showSlide(index);
 
   next.addEventListener("click", () => {
     index++;
-    console.log('index:', index)
     showSlide(index);
   });
   prev.addEventListener("click", () => {
     index--;
     showSlide(index);
   });
+
+  // dots
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => {
+      index = i;
+      showSlide(index);
+    });
+  }
 };
 
 export { slideshow };
